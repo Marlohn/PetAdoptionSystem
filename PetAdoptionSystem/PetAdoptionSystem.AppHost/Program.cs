0af0e2978a72e0aa3e0c34c34b98cc5d19bb8b41
Marlohn Choinski <marlohn.choinski@hotmail.com>
2024-06-18 09:33:26 -0300

    rename api project

--- PetAdoptionSystem/PetAdoptionSystem.AppHost/Program.cs
+++ PetAdoptionSystem/PetAdoptionSystem.AppHost/Program.cs
@@ -1,6 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.PetAdoptionSystem_ApiService>("apiservice");
+var apiService = builder.AddProject<Projects.PetAdoptionSystem_Api>("apiservice");
 
 builder.AddProject<Projects.PetAdoptionSystem_Web>("webfrontend")
     .WithExternalHttpEndpoints()
