768d5273de303c333d1988648dceb476ead1b91d
Marlohn Choinski <marlohn.choinski@hotmail.com>
2024-06-18 10:44:04 -0300

    add new api service

--- PetAdoptionSystem/PetAdoptionSystem.AppHost/Program.cs
+++ PetAdoptionSystem/PetAdoptionSystem.AppHost/Program.cs
@@ -1,6 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.PetAdoptionSystem_Api>("apiservice");
+var apiService = builder.AddProject<Projects.PetAdoptionSystem_ApiService>("apiservice");
+
+var api = builder.AddProject<Projects.PetAdoptionSystem_Api>("api");
 
 builder.AddProject<Projects.PetAdoptionSystem_Web>("webfrontend")
     .WithExternalHttpEndpoints()
