a8531d90e3ddfca200aea4f6482b0cdd7d7bdb1d
Marlohn Choinski <marlohn.choinski@konabos.com>
2024-06-20 16:07:00 -0300

    add validations

--- PetAdoptionSystem/PetAdoptionSystem.Application/Dtos/PetRequestDto.cs
+++ PetAdoptionSystem/PetAdoptionSystem.Application/Dtos/PetRequestDto.cs
@@ -1,10 +1,23 @@
-﻿namespace PetAdoptionSystem.Application.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetAdoptionSystem.Application.Dtos
 {
     public class PetRequestDto
     {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, ErrorMessage = "Name length can't be more than 50.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Type is required.")]
+        [StringLength(50, ErrorMessage = "Type length can't be more than 50.")]
         public string Type { get; set; }
+
+        [Required(ErrorMessage = "Breed is required.")]
+        [StringLength(50, ErrorMessage = "Breed length can't be more than 50.")]
         public string Breed { get; set; }
+
+        [Required(ErrorMessage = "Sex is required.")]
+        [RegularExpression("^(Male|Female)$", ErrorMessage = "Sex must be either 'Male' or 'Female'.")]
         public string Sex { get; set; }
     }
 }
