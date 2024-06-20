930a9d95a7dd463cc404b09a3e500ed3a38abf58
Marlohn Choinski <marlohn.choinski@konabos.com>
2024-06-20 17:44:46 -0300

    Clean up

--- PetAdoptionSystem/PetAdoptionSystem.IoC/Extensions/ServiceCollectionExtensions.cs
+++ PetAdoptionSystem/PetAdoptionSystem.IoC/Extensions/ServiceCollectionExtensions.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System.Text;
 
 namespace PetAdoptionSystem.IoC.Extensions
 {
